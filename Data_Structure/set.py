d9e18a6718e7da9e2c119594dab7427b0dd61b5b
akhil <akhil1.webappdev@gmail.com>
2025-08-08 10:08:58 +0530

    with git ignore

--- Data_Structure/set.py
+++ Data_Structure/set.py
@@ -1,2 +1,26 @@
-thisset = {'a','b','c','d'}
-print(thisset)
+# thisset = {'a','b','c','d'}
+# print(thisset)
+
+
+
+import copy
+thislist = []
+
+
+shallowcoppy = copy.copy(thislist)
+thislist[1] = "hello"
+
+shallowcoppy[0] = "hdgj"
+print(shallowcoppy)
+print(thislist)
+
+
+
+
+
+
+
+deepcopy = copy.deepcopy(thislist)
+
+
+
